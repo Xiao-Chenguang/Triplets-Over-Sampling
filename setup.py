687e1ec23f9be7d73ef5bcf966e29545892f5577
Xiao-Chenguang <973990275@qq.com>
2024-05-08 10:45:09 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tos",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+    ],
+)
